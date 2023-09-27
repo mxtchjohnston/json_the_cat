37b7af34f358034901cc7c943d6cc4f7534f36a5
mxtchjohnston <mitchjohnston99@gmail.com>
2023-09-27 21:50:09 +0000

    edge cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,10 +1,19 @@
 const request = require('request');
 
 const endpoint = 'https://api.thecatapi.com/v1/breeds/search';
-const breed = "Siberian";
+const breed = process.argv[2];
 
 request(`${endpoint}?q=${breed}`, (error, response, body) => {
-  if(error) console.log(error);
+  //console.log('error:', error);
+  //console.log('response:', response)
+  if(error) {
+    console.log(error);
+    return;
+  }
   let obj = JSON.parse(body);
-  console.log(obj);
+  if (obj[0]) {
+    console.log(obj[0].description);
+    return;
+  } 
+  console.log('Breed not found:', breed);
 });
