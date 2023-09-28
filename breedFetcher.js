caeb83b201e70a3eb8706851fb489e869d640853
mxtchjohnston <mitchjohnston99@gmail.com>
2023-09-28 20:20:19 +0000

    test cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,24 @@
 const request = require('request');
 
-const endpoint = 'https://api.thecatapi.com/v1/breeds/search';
-const breed = process.argv[2];
 
-request(`${endpoint}?q=${breed}`, (error, response, body) => {
-  //console.log('error:', error);
-  //console.log('response:', response)
-  if (error) {
-    console.log(error);
-    return;
-  }
-  let obj = JSON.parse(body);
-  if (obj[0]) {
-    console.log(obj[0].description);
-    return;
-  }
-  console.log('Breed not found:', breed);
-});
+const fetchBreedDescription = (breed, callback) => {
+  const endpoint = 'https://api.thecatapi.com/v1/breeds/search';
+  //const breed = process.argv[2];
+
+  request(`${endpoint}?q=${breed}`, (error, response, body) => {
+    //console.log('error:', error);
+    //console.log('response:', response)
+    if (error) {
+      console.log(error);
+      return;
+    }
+    let obj = JSON.parse(body);
+    if (obj[0]) {
+      callback(null, obj[0].description);
+    }
+  });
+};
+
+//fetchBreedDescription('Siberian', (err, res) => console.log(res));
+
+module.exports = fetchBreedDescription;
